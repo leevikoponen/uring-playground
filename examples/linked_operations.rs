49b38835fcb0af243c124704b66452b3675f8c8a
Leevi Koponen <contact@leevikoponen.fi>
2024-11-24 12:42:27 +0200

    Add block_on helper function

--- examples/linked_operations.rs
+++ examples/linked_operations.rs
@@ -1,21 +1,12 @@
 //! Example showcasing both basic usage and how we have the ability to submit
 //! multiple operations that are linked together.
-use std::cell::RefCell;
+use std::{cell::RefCell, io::Result};
 
-use futures_lite::future;
 use uring_playground::{batch::Link2, future::SubmitAndWait, operation::Nop, reactor::Reactor};
 
-fn main() {
-    let reactor = Reactor::new(64).map(RefCell::new).unwrap();
-    future::block_on(future::or(
-        async {
-            SubmitAndWait::new(&reactor, Link2::new(Nop::new(), Nop::new())).await;
-        },
-        async {
-            loop {
-                reactor.borrow_mut().wait_for_progress(None).unwrap();
-                future::yield_now().await;
-            }
-        },
-    ));
+fn main() -> Result<()> {
+    let reactor = Reactor::new(64).map(RefCell::new)?;
+    uring_playground::block_on(&reactor, async {
+        SubmitAndWait::new(&reactor, Link2::new(Nop::new(), Nop::new())).await;
+    })
 }
--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,28 @@
 //! It should be fine, but working with raw OS primitives is always somewhat
 //! easy to get wrong, especially with how `io_uring` largely centers around the
 //! notion of temporarily transferring ownership to the kernel.
+use std::{cell::RefCell, future::IntoFuture, io::Result};
+
+use crate::reactor::Reactor;
+
 pub mod batch;
 pub mod future;
 pub mod operation;
 pub mod reactor;
+
+/// Block on the future by polling it concurrently with driving the reactor.
+///
+/// # Errors
+///
+/// If [`Reactor::wait_for_progress`] results in an error.
+pub fn block_on<T>(reactor: &RefCell<Reactor>, future: impl IntoFuture<Output = T>) -> Result<T> {
+    futures_lite::future::block_on(futures_lite::future::or(
+        async { Ok(future.await) },
+        async {
+            loop {
+                reactor.borrow_mut().wait_for_progress(None)?;
+                futures_lite::future::yield_now().await;
+            }
+        },
+    ))
+}
