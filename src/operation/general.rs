c08f041abcac8b02e6a06749c3eccb43af91d08d
Leevi Koponen <contact@leevikoponen.fi>
2024-11-25 12:03:54 +0200

    Add link timeout operation

--- src/operation/general.rs
+++ src/operation/general.rs
@@ -1,6 +1,15 @@
-use std::any::Any;
+use std::{
+    any::Any,
+    io::{Error, Result},
+    time::{Duration, SystemTime, UNIX_EPOCH},
+};
 
-use io_uring::{cqueue, opcode, squeue};
+use io_uring::{
+    cqueue,
+    opcode,
+    squeue,
+    types::{TimeoutFlags, Timespec},
+};
 
 use crate::operation::{Oneshot, Operation};
 
@@ -36,3 +45,57 @@ unsafe impl Operation for Nop {
 
 // SAFETY: only returns once
 unsafe impl Oneshot for Nop {}
+
+/// Operation that connects a timeout to linked operations.
+///
+/// Corresponds to [io_uring_prep_link_timeout(3)](https://www.man7.org/linux/man-pages/man3/io_uring_prep_link_timeout.3.html).
+#[must_use]
+pub struct LinkTimeout {
+    time: Timespec,
+    flags: TimeoutFlags,
+}
+
+impl LinkTimeout {
+    pub fn relative(duration: Duration) -> Self {
+        Self {
+            time: Timespec::from(duration),
+            flags: TimeoutFlags::empty(),
+        }
+    }
+
+    pub fn absolute(time: SystemTime) -> Self {
+        Self {
+            time: time
+                .duration_since(UNIX_EPOCH)
+                .map(Timespec::from)
+                .unwrap_or_default(),
+            flags: TimeoutFlags::ABS | TimeoutFlags::REALTIME,
+        }
+    }
+}
+
+// SAFETY: no parameters to invalidate
+unsafe impl Operation for LinkTimeout {
+    type Output = Result<()>;
+
+    fn build_submission(&mut self) -> squeue::Entry {
+        opcode::LinkTimeout::new(&self.time)
+            .flags(self.flags)
+            .build()
+    }
+
+    unsafe fn handle_completion(&mut self, entry: cqueue::Entry) -> Self::Output {
+        if entry.result().is_negative() {
+            return Err(Error::from_raw_os_error(-entry.result()));
+        }
+
+        Ok(())
+    }
+
+    fn take_required_allocations(&mut self) -> Option<Box<dyn Any>> {
+        None
+    }
+}
+
+// SAFETY: only returns once
+unsafe impl Oneshot for LinkTimeout {}
--- src/operation/mod.rs
+++ src/operation/mod.rs
@@ -9,7 +9,7 @@ mod wrapper;
 pub use self::{
     definition::{Batch, Oneshot, Operation},
     future::SubmitAndWait,
-    general::Nop,
+    general::{LinkTimeout, Nop},
     link::{Link2, Link3, Link4, Link5},
     synchronization::{FutexWait, FutexWake},
     wrapper::{MapOutput, Single, StashOutput},
